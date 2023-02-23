1f5e1a83e192bc6906ba1836ae4215596c234172
Melque Henrique <melquehl@gmail.com>
2023-02-23 17:57:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sorteionumerico/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sorteionumerico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SortearNumero(View view){
+
+        TextView textoResultado = findViewById(R.id.textoResultado);
+
+        int numeroAleatorio = new Random().nextInt(11);
+
+        textoResultado.setText("Numero sorteado foi: "+ numeroAleatorio);
+
+    }
+}
